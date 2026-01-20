f089603f1be1bff4527c3733a28603f71dad6ea2
cadudesigner-777 <cadudesigner2008@gmail.com>
2026-01-20 08:35:39 -0300

    Ajuste regra

--- calcula.php
+++ calcula.php
@@ -5,13 +5,19 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 $nome = $_POST['nome'];
 $tarefas = $_POST['tarefas'];
 $atrasos = $_POST['atrasos'];
+$setor = $_POST['setor'];
 
 if($tarefas >= 25 && $atrasos == 0){
     $situacao = "Funcionario elegivel para bônus";
+
 } elseif($tarefas >= 10 && $tarefas <= 19 && $atrasos <= 3){
+
     $situacao = "Desempenho regular";
+
 } else{
+
     $situacao = "Desempenho insatisfatório";
+
 }
 
 
@@ -20,6 +26,7 @@ echo "<h3>Resultado da avaliação</h3> <br>";
 echo "Nome: $nome <br>";
 echo "Suas tarefas: $tarefas <br>";
 echo "Seus atrasos: $atrasos <br>";
+echo "Seus setor: $setor <br>";
 echo "Sua situação: $situacao <br>";
 }
 ?>
